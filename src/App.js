e23914f38aa208a2551f0307fb57ef34c0bbe01c
Lav Kumar Singh <lavkumarsingh@GELAP132.local>
2023-12-23 01:54:19 +0530

    init

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Button } from "antd";
 import axios from "axios";
 import { useSearchParams } from 'react-router-dom'
 import "./App.css"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const BASE_URL = "https://api.upstox.com/v2";
 const CLIENT_ID = "9891272d-18cf-4a88-a7c2-af59a5c15d01";
@@ -13,22 +13,10 @@ function App() {
   const url = `${BASE_URL}/login/authorization/dialog?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${"code"}`;
   
   const [searchParams, setSearchParams] = useSearchParams();
-  const code = searchParams.get("code");
   const [token, setToken] = useState();
 
-  const getToken = async () => {
-    axios.post(`${BASE_URL}/login/authorization/token?code=${code}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=${REDIRECT_URI}&grant_type=${"authorization_code"}`, {}, {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Api-Version": "2.0"
-      }
-    }).then(res => {
-      console.log(res.data);
-      setToken(res.data.access_token)
-    })
-  }
-
-  const getAuth = async () => {
+  useEffect(() => {
+    const code = searchParams.get("code");
     if(code)
     axios.post(`${BASE_URL}/login/authorization/token?code=${code}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=${REDIRECT_URI}&grant_type=${"authorization_code"}`, {}, {
       headers: {
@@ -39,18 +27,13 @@ function App() {
       console.log(res.data);
       setToken(res.data.access_token)
     })
-  }
+  }, []);
 
   return (
     <div className="App">
       <div style={{display: "flex", justifyContent: "center"}}>
         <Button onClick={getAuth} href={url}>Login</Button>
-        <p>{code}</p>
       </div>
-      {/* <div style={{display: "flex", justifyContent: "center"}}>
-        <Button onClick={getToken}>Token</Button>
-        <p>{token}</p>
-      </div> */}
     </div>
   );
 }
