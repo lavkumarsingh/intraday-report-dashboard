db56b26c8bfeda2291d6cb8c11682fc94adbe206
Lav Kumar Singh <lavkumarsingh@GELAP132.local>
2023-12-23 04:44:52 +0530

    init

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -2,8 +2,12 @@ function Dashboard() {
     console.log(JSON.parse(localStorage.getItem('auth')));
     return ( 
         <>
-        Dashboard
-        {JSON.parse(localStorage.getItem('auth'))?.user_name}
+        <h4>Logged in</h4>
+        <p>Name: {JSON.parse(localStorage.getItem('auth'))?.user_name}</p>
+        <p>Email: {JSON.parse(localStorage.getItem('auth'))?.email}</p>
+        <p>User ID: {JSON.parse(localStorage.getItem('auth'))?.user_id}</p>
+        <p>Access Token: {JSON.parse(localStorage.getItem('auth'))?.access_token}</p>
+        <p>Broker: {JSON.parse(localStorage.getItem('auth'))?.UPSTOX}</p>
         </>
      );
 }
